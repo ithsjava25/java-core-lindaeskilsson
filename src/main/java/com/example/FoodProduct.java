feb2e3e73fc38a9494ecd85236d91473a4efb1c3
Linda Eskilsson <eskilsson.linda@hotmail.se>
2025-10-20 10:13:37 +0200

    implemented subclass foodProduct

--- src/main/java/com/example/FoodProduct.java
+++ src/main/java/com/example/FoodProduct.java
@@ -1,4 +1,25 @@
 package com.example;
 
-public class FoodProduct {
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+// subklass för matprodukter som extendar product. Lägger till utgångsdatum med hjälp av localdate.
+
+public class FoodProduct extends Product {
+    private final LocalDate bestBefore;
+
+    public FoodProduct(String name, LocalDate bestBefore, Category category, double price, LocalDate bestBefore1)
+    {
+        super(name, category, BigDecimal.valueOf(price));
+        this.bestBefore = bestBefore;
+    }
+
+    public LocalDate getBestBefore() {
+        return bestBefore;
+    }
+
+    @Override
+    public String productDetails() {
+        return "";
+    }
 }
